85531c8e8693dd9f3510f5a55ff44a64783df834
sorias <soria.sandra.m@gmail.com>
2023-06-23 12:09:33 +0200

    Show countries by continent

--- my/src/main.tsx
+++ my/src/main.tsx
@@ -7,6 +7,7 @@ import './index.css'
 import { loadCountries} from './components/Countries';
 import { loadWeather} from './components/Weather';
 import { renderCapitals} from './components/Render';
+//import axios from 'axios';
 
 const continents = ['Asia', 'Africa', 'North America', 'South America', 'Antarctica', 'Europe', 'Australia' ];
 
@@ -29,11 +30,19 @@ export  function LaLista( continent: String ) {
   let html = [];
   for( const c of countries) {
     if(c.continents[0] == continent){
-      html.push(<button>{c.name}</button>)
+      html.push(<button><p class="pais">{c.name}</p><p>{c.capitals}</p></button>)
     } 
   }
   return html;
 }  
+
+export  function allContinents() {
+  let html = [];
+  for( const c of continents) {
+    html.push(<button>{c}</button>)
+  }
+  return html;
+} 
 
 /*ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <ul>{html}</ul>
@@ -42,44 +51,86 @@ export  function LaLista( continent: String ) {
 class TodosPaises extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {lista: LaLista('Europe')};
+    this.state = {listContinents: LaLista('Europe')};
+  }
+  handleClick = (e) =>{
+    e.preventDefault();
+    console.log('Sandraa')
+    this.setState(prevState => ({listContinents: LaLista('Europe')}))
   }
   render() {
     return (<>
-      <h1 style={{color: this.props.color}}>Sandraa</h1>
-      <h2>{this.state.lista}</h2>
-    </>);
+      <p>arnau</p>
+      <a href='#' onClick={this.handleClick}> Click me
+      {this.state.listContinents && <TodosPaises/>} </a></>);
   }
-  componentDidMount(): void {
-    const filtrar = () => {this.setState({lista: LaLista('Asia')})}
-    this.timer = setInterval(filtrar, 1000)
+  async componentDidMount(): Promise<void> {
+    //const res = await axios.get("api")
+    
+    const filtrar = () => {this.setState({listContinents: LaLista('Europe')})}
+    //this.timer = setInterval(filtrar, 1000)
   }
   componentDidUpdate(): void {
-    clearInterval(this.timer)
+
   }
 }
-  
- 
-  
-ReactDOM.createRoot(document.getElementById('root')).render(<TodosPaises color="red"/>)
 
-/*class ActionLink extends React.Component {
+class Asia extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { relojVisible: false};
+    this.state = { rlistContinents: allContinents()};
   };
   handleClick = (e) =>{
     e.preventDefault();
-    this.setState(prevState => ({relojVisible: !prevState.relojVisible}))
+    this.setState(prevState => ({listContinents: LaLista('Asia')}))
   }
   render() {
-    return (
-      <a href='#' onCick={this.handleClick}> Click me
-      {this.state.relojVisible && <Clock/>} </a>
+    return (<>
+              <a href='#' onClick={this.handleClick}> Asia
+              {this.state.listContinents} </a>
+            </>
       );
   }
 }
-*/
+class Europe extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { rlistContinents: allContinents()};
+  };
+  handleClick = (e) =>{
+    e.preventDefault();
+    this.setState(prevState => ({listContinents: LaLista('Europe')}))
+  }
+  render() {
+    return (<>
+              <a href='#' onClick={this.handleClick}> Europa
+              {this.state.listContinents}</a>
+            </>
+      );
+  }
+}
+
+class ActionLink extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { rlistContinents: allContinents()};
+  };
+  handleClick = (e) =>{
+    e.preventDefault();
+    this.setState(prevState => ({listContinents: allContinents()}))
+  }
+  render() {
+    return (<>
+      
+      <a href='#' onClick={this.handleClick}>{<Asia/>}</a>
+      <p></p>
+      <a href='#' onClick={this.handleClick}>{<Europe/>}</a>
+      </>
+      );
+  }
+}
+ReactDOM.createRoot(document.getElementById('root')).render(<ActionLink/>)
+//ReactDOM.createRoot(document.getElementById('root')).render(<TodosPaises color="red"/>)
 /*ReactDOM.createR(document.getElementById('root') as HTMLElement).render(
   <>
     <div className='estilo1'>
